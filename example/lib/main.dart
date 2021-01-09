68ea7e54339347ca78270beb69d4af225641bf98
Andres Rojas <afro17@gmail.com>
2021-01-09 18:07:22 -0500

    Add countdown widget and example project (#1)
    * Add example project

---
+++ example/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:countdown_progress_indicator/countdown_progress_indicator.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isRunning = true;
+  final _controller = CountDownController();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: CountDownProgressIndicator(
+                    controller: _controller,
+                    valueColor: Colors.red,
+                    backgroundColor: Colors.blue,
+                    initialPosition: 0,
+                    duration: 20,
+                    text: 'SEC',
+                    onComplete: () => null,
+                  ),
+                ),
+                SizedBox(height: 20),
+                RaisedButton(
+                  onPressed: () => setState(() {
+                    if (_isRunning)
+                      _controller.pause();
+                    else
+                      _controller.resume();
+
+                    _isRunning = !_isRunning;
+                  }),
+                  child: Text(_isRunning ? 'Pause' : 'Resume'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
