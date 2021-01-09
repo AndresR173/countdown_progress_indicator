083d379bd6b525d1b2aed1ece3694e5fd7a09b5c
Andres Rojas <afro17@gmail.com>
2021-01-09 15:16:31 -0500

    cleanup

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:countdown_progress_indicator/countdown_progress_indicator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: SizedBox(
+              height: 200,
+              width: 200,
+              child: CountDownProgressIndicator(
+                valueColor: Colors.red,
+                backgroundColor: Colors.blue,
+                time: 20,
+                onFinish: (_) => null,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
